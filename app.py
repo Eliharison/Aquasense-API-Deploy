5b4ef8ea7dbea17da53203614edd3e79717b40af
Eliharison Gabriel <124097279+Eliharison@users.noreply.github.com>
2024-11-23 14:56:17 -0300

    Feat: Adding wsgi.py

--- app.py
+++ app.py
@@ -6,17 +6,14 @@ from config import Config
 
 app = Flask(__name__)
 
+# Configurações
 app.config.from_object(Config)
 
+# Inicializações
 initialize_db(app)
-
 Extensions.initialize_jwt(app)
 Extensions.initialize_marsh(app)
 Extensions.initialize_bcript(app)
 
-
+# Registro de rotas
 app.register_blueprint(routes)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from app import app
+
+# Expondo a aplicação Flask
+app = app
